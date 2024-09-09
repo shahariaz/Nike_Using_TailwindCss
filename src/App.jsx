2d762315010c2bb1d7d3c466f724e2c0a65cbc90
shahariaz <shahariaz.info@gmail.com>
2024-09-09 22:53:57 +0600

    app

--- src/App.jsx
+++ src/App.jsx
@@ -1,3 +1,17 @@
-export default function App() {
-  return <h1 className="text-3xl font-bold underline">Hello world</h1>;
-}
+const App = () => {
+  return (
+    <main className=" relative">
+      Nav
+      <section className=" xl:padding-1 wide:padding-r padding-b">Hero</section>
+      <section className=" padding">PopulerProducts</section>
+      <section className=" padding">SuperQuality</section>
+      <section className=" padding-x py-10">Services</section>
+      <section className=" padding">SpecialOffers</section>
+      <section className=" padding">CoustomerReviews</section>
+      <section className=" padding">Subscribe</section>
+      <section className=" padding">footer</section>
+    </main>
+  );
+};
+
+export default App;
---
+++ src/script.js
@@ -0,0 +1,14 @@
+<script type="text/javascript">
+  document.addEventListener("DOMContentLoaded", () => {
+    const toggleDark = document.getElementById('toggleDark')
+    toggleDark.addEventListener('click', function() {
+      if(document.documentElement.classList.includes('dark')) {
+        document.documentElement.classList.remove('dark')
+      }
+      else {
+        document.documentElement.classList.add('dark')
+      }
+      alert("click!")
+    });
+  });
+</script>
